d5dc4f88ec28a16dd5b1a99a13086d66aa17fad1
Adam Månsson <adam.s.mansson@gmail.com>
2022-12-02 12:36:47 +0100

    Improve day 2 solution a bit

--- src/main/scala/aoc/2022/02-rock-paper-scissors/Part1.scala
+++ src/main/scala/aoc/2022/02-rock-paper-scissors/Part1.scala
@@ -11,16 +11,16 @@ object Part1 extends Problem(2, 2022)(1)(15):
         right match
           case "X" => 3 + 1
           case "Y" => 6 + 2
-          case "Z" => 0 + 3
+          case _ => 0 + 3
       case "B" =>
         right match
           case "X" => 0 + 1
           case "Y" => 3 + 2
-          case "Z" => 6 + 3
-      case "C" =>
+          case _ => 6 + 3
+      case _ =>
         right match
           case "X" => 6 + 1
           case "Y" => 0 + 2
-          case "Z" => 3 + 3
+          case _ => 3 + 3
           
   def solve(data: List[String]) = parse(data).map(xs => outcome(xs(0), xs(1))).sum
--- src/main/scala/aoc/2022/02-rock-paper-scissors/Part2.scala
+++ src/main/scala/aoc/2022/02-rock-paper-scissors/Part2.scala
@@ -11,16 +11,16 @@ object Part2 extends Problem(2, 2022)(2)(12):
         right match
           case "X" => 0 + 3
           case "Y" => 3 + 1
-          case "Z" => 6 + 2
+          case _ => 6 + 2
       case "B" =>
         right match
           case "X" => 0 + 1
           case "Y" => 3 + 2
-          case "Z" => 6 + 3
-      case "C" =>
+          case _ => 6 + 3
+      case _ =>
         right match
           case "X" => 0 + 2
           case "Y" => 3 + 3
-          case "Z" => 6 + 1
+          case _ => 6 + 1
 
   def solve(data: List[String]) = parse(data).map(xs => outcome(xs(0), xs(1))).sum
