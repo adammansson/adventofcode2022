2fe3c73b5ea18c430107c917a7c279c140f77868
Adam Månsson <adam.s.mansson@gmail.com>
2022-12-07 15:05:16 +0100

    Improve solution for day 7 a bit

--- src/main/scala/aoc/2022/07-no-space-left-on-device/Part2.scala
+++ src/main/scala/aoc/2022/07-no-space-left-on-device/Part2.scala
@@ -9,36 +9,11 @@ import scala.collection.mutable.ArrayBuffer
 object Part2 extends Problem(7, 2022)(2)(24933642):
   def name = "No Space Left On Device - Part 2"
   def solve(data: List[String]): Int =
-    val dirs = mutable.Stack[String]()
-    val dirSizes = mutable.Map[String, mutable.Map[String, Int]]()
-
-    for line <- data do
-      val split = line.split(" ").toVector
-
-      if split(0) == "$" then
-        if  split(1) == "cd" then
-          if split(2) == ".." then
-            dirs.pop()
-          else
-            dirs.push(split(2))
-
-      else
-        if split(0) != ("dir") then
-          val fSize = split(0).toInt
-          val fName = split(1)
-          val filePath = fName + "/" + dirs.mkString("/")
-
-          for i <- dirs.indices do
-            val dirPath = dirs.toVector.drop(i).mkString("/")
-            val files = dirSizes.getOrElse(dirPath, mutable.Map[String, Int]())
-            if !files.isDefinedAt(filePath) then
-              dirSizes += (dirPath -> (files += (filePath -> fSize)))
-
+    val dirSizes = parse(data)
     val total = 70000000
     val neededForUpdate = 30000000
-    //val used = dirSizes.map((_, f) => f.values.sum).sum
     val used = dirSizes("/").values.sum
     val unused = total - used
     val mustBeDeleted = neededForUpdate - unused
-    println(mustBeDeleted)
+    
     dirSizes.map((_, f) => f.values.sum).filter(i => i >= mustBeDeleted).toVector.sorted.head
--- src/main/scala/aoc/2022/07-no-space-left-on-device/package.scala
+++ src/main/scala/aoc/2022/07-no-space-left-on-device/package.scala
@@ -1,4 +1,33 @@
 package aoc.y2022.day07
-import problemutils.*, extensions.*
+import problemutils.*
+import extensions.*
 
-def parse(data: List[String]) = ???
+import scala.collection.mutable
+
+def parse(data: List[String]): Map[String, Map[String, Int]] =
+  val dirs = mutable.Stack[String]()
+  val dirSizes = mutable.Map[String, mutable.Map[String, Int]]()
+
+  for line <- data do
+    val split = line.split(" ").toVector
+
+    if split(0) == "$" then
+      if  split(1) == "cd" then
+        if split(2) == ".." then
+          dirs.pop()
+        else
+          dirs.push(split(2))
+
+    else
+      if split(0) != ("dir") then
+        val fSize = split(0).toInt
+        val fName = split(1)
+        val filePath = fName + "/" + dirs.mkString("/")
+
+        for i <- dirs.indices do
+          val dirPath = dirs.toVector.drop(i).mkString("/")
+          val files = dirSizes.getOrElse(dirPath, mutable.Map[String, Int]())
+          if !files.isDefinedAt(filePath) then
+            dirSizes += (dirPath -> (files += (filePath -> fSize)))
+
+  dirSizes.map((k, v) => (k -> v.toMap)).toMap
