f851841153ee25075b2f78ba7aa425021b63cad4
Adam Månsson <adam.s.mansson@gmail.com>
2022-12-07 15:40:34 +0100

    Improve day 7 solution using match case

--- src/main/scala/aoc/2022/07-no-space-left-on-device/package.scala
+++ src/main/scala/aoc/2022/07-no-space-left-on-device/package.scala
@@ -9,17 +9,17 @@ def parse(data: List[String]): Map[String, Map[String, Int]] =
   val dirSizes = mutable.Map[String, mutable.Map[String, Int]]()
 
   for line <- data.map(_.split(" ").toVector) do
-    if line(0) == "$" then
-      if  line(1) == "cd" then
-        if line(2) == ".." then
-          dirs.pop()
-        else
-          dirs.push(line(2))
+    line match
+      case Vector("$", cmd, arg) =>
+        cmd match
+          case "cd" =>
+            arg match
+              case ".." => dirs.pop()
+              case _ => dirs.push(line(2))
 
-    else
-      if line(0) != ("dir") then
-        val fSize = line(0).toInt
-        val fName = line(1)
+      case Vector(s0, s1) if s0.forall(_.isDigit) =>
+        val fSize = s0.toInt
+        val fName = s1
         val filePath = fName + "/" + dirs.mkString("/")
 
         for i <- dirs.indices do
@@ -28,4 +28,6 @@ def parse(data: List[String]): Map[String, Map[String, Int]] =
           if !files.isDefinedAt(filePath) then
             dirSizes += (dirPath -> (files += (filePath -> fSize)))
 
-  dirSizes.map((k, v) => (k -> v.toMap)).toMap
+      case _ =>
+
+  dirSizes.map((k, v) => k -> v.toMap).toMap
