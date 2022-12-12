f556241e2a1d61c1ed905fca81a3e8380d483ce1
Adam Månsson <adam.s.mansson@gmail.com>
2022-12-12 19:38:05 +0100

    Improve day 10 part 1

--- src/main/scala/aoc/2022/10-cathode-ray-tube/Part1.scala
+++ src/main/scala/aoc/2022/10-cathode-ray-tube/Part1.scala
@@ -5,25 +5,14 @@ import problemutils.*, extensions.*
 object Part1 extends Problem(10, 2022)(1)(13140):
   def name = "Cathode-Ray Tube - Part 1"
   def solve(data: List[String]): Int =
-    var counter = 0
-    var reg = 1
-    var res = 0
+    var register = 1
+    parse(data).zipWithIndex.map((line, i) => (line, i + 1)).map((line, i) => {
+      var result = 0
+      if (i - 20) % 40 == 0 then
+        result = i * register
 
-    for line <- parse(data) do
       if line(0) == "addx" then
-        counter += 1
-        if counter == 20 || (counter - 20) % 40 == 0 then
-          println(counter * reg)
-          res += counter * reg
-        counter += 1
-        if counter == 20 || (counter - 20) % 40 == 0 then
-          println(counter * reg)
-          res += counter * reg
-        reg += line(1).toInt
-      else
-        counter += 1
-        if counter == 20 || (counter - 20) % 40 == 0 then
-          println(counter * reg)
-          res += counter * reg
+        register += line(1).toInt
 
-    res
+      result
+    }).sum
--- src/main/scala/aoc/2022/10-cathode-ray-tube/package.scala
+++ src/main/scala/aoc/2022/10-cathode-ray-tube/package.scala
@@ -1,4 +1,5 @@
 package aoc.y2022.day10
 import problemutils.*, extensions.*
 
-def parse(data: List[String]): List[Vector[String]] = data.map(s => s.split(" ").toVector)
+// def parse(data: List[String]): List[Vector[String]] = data.map(s => s.split(" ").toVector)
+def parse(data: List[String]): List[Vector[String]] = data.map(s => s.split(" ").toVector).flatMap(a => if (a(0) == "addx") Vector(Vector("noop"), a) else Vector(a))
